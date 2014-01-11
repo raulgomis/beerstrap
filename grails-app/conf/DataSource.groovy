39ffe2a497fd3088ac9fa9f2bc0c3e1fffbfd77a
Raul Gomis <raulgomis@hotmail.com>
2014-01-11 01:57:30 +0100

    In memory database for production

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -29,7 +29,7 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:h2:mem:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
             //driverClassName= 'com.mysql.jdbc.Driver'
             //dbCreate = "update"
             //dialect= org.hibernate.dialect.MySQLInnoDBDialect
